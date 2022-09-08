f31ea0599d14a54d9ab367a8d6089ab48a5aca80
armiii06 <112963042+armiii06@users.noreply.github.com>
2022-09-07 20:52:31 -1200

    Add files via upload

---
+++ ARMIYUSNITA_C.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int R;
+	cout<<"Angka Pertama    :";
+	cin>>R;
+	
+	int S;
+	cout<<"Angka Kedua      :";
+	cin>>S;
+	
+	cout << R + S <<endl;
+	cout << R - S <<endl;
+	cout << R * S <<endl;
+	cout << R / S <<endl;
+	cout << R % S <<endl;
+	
+	return 0;
+}
